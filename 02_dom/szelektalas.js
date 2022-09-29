46482b97c925fa11fa59e7438fab36f05a5c4009
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-09-29 09:18:57 +0200

    balazs.matyassy: dom összefoglaló

---
+++ 02_dom/szelektalas.js
@@ -0,0 +1,24 @@
+// 1 elem lekérdezése id alapján
+let elem = document.getElementById('azonosito');
+
+// 1 elem lekérdezése css szelektor alapján
+// több találat esetén az első kerül lekérdezésre
+let elem = document.querySelector('#azonosito');
+let elem = document.querySelector('.osztaly');
+let elem = document.querySelector('h2');
+
+// több elem lekérdezése css szelektor alapján
+// az eredmény tömbként kerül visszaadásra
+let elemek = document.querySelectorAll('.osztaly');
+let elemek = document.querySelectorAll('h2');
+
+// szöveges elemek (div, span, p, h1, h2, li, td, th...)
+// tartalma az innerHTML tulajdonság segítségével írható / olvasható
+
+// Pl. tartalom kétszerezése
+// += rövidítés használható
+elem.innerHTML = elem.innerHTML + elem.innerHTML;
+
+// bemeneti elemek (input...) tartalma a value tulajdonság segítségével írható / olvasható
+let input = document.getElementById('bemenet');
+input.value = input.value + input.value;
